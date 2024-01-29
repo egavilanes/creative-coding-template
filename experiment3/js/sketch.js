2c5e31dc95ca1062c663280e9bc81d41967717a6
Emily Gavilanes <108209710+egavilanes@users.noreply.github.com>
2024-01-29 15:45:53 -0800

    imitate changes

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -25,43 +25,83 @@ class MyClass {
     }
 }
 
+
+'use strict';
+
+var shapes = [];
+var density = 9;
+var shapeHeight = 26;
+var shapeColor;
+
+var newShape;
+
 // setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    createCanvas(800, 800);
+    noFill();
+    shapeColor = color(0);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(255);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    shapes.forEach(function(shape) {
+      shape.draw();
+    });
+  
+    if (newShape) {
+      newShape.x2 = mouseX;
+      newShape.y2 = mouseY;
+      newShape.h = shapeHeight;
+      newShape.c = shapeColor;
+      newShape.draw();
 }
+}
+
+function Shape(x1, y1, x2, y2, h, c) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.h = h;
+    this.c = c;
+  
+    Shape.prototype.draw = function() {
+      var w = dist(this.x1, this.y1, this.x2, this.y2);
+      var a = atan2(this.y2 - this.y1, this.x2 - this.x1);
+      stroke(this.c);
+      push();
+      translate(this.x1, this.y1);
+      rotate(a);
+      translate(0, -this.h / 2);
+      for (var i = 0; i < this.h; i += density) {
+        line(0, i, w, i);
+      }
+      pop();
+    };
+  }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
     // code to run when mouse is pressed
+    newShape = new Shape(pmouseX, pmouseY, mouseX, mouseY, shapeHeight, shapeColor);
 }
+
+function mouseReleased() {
+    shapes.push(newShape);
+    newShape = undefined;
+  }
+  
+  function keyPressed() {
+    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  
+    if (key == '1') shapeColor = color(255, 0, 0);
+    if (key == '2') shapeColor = color(0, 255, 0);
+    if (key == '3') shapeColor = color(0, 0, 255);
+    if (key == '4') shapeColor = color(0);
+  
+    if (keyCode == UP_ARROW) shapeHeight += density;
+    if (keyCode == DOWN_ARROW) shapeHeight -= density;
+  }
+  
